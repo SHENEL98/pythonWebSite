89a42a7b1df76b81954c2a89aa847912d095675d
Shenel Kurera <61915302+SHENEL98@users.noreply.github.com>
2023-03-13 14:44:46 +0000

    adding dynamic data

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id':1,
+    'title':'Data',
+    'type':'perment',
+    'salary':'Rs.20000'
+  },
+  {
+    'id':2,
+    'title':'Data-2',
+    'type':'perment',
+    'salary':'Rs.20000'
+  },
+  {
+    'id':3,
+    'title':'Data-3',
+    'type':'perment',
+    'salary':'Rs.20000'
+  }
+]
+
 @app.route("/")
 def helloworld():
-  return render_template('home.html')
+  return render_template('home.html',jobs=JOBS)
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   # print("indise the if now")
